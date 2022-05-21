229bc511ea13aca01760080f69ebb0b6fc4f7db2
mhalg <mhalg@ya.ru>
2022-05-21 23:13:50 +0500

    Homework final version for lesson2

--- testscript.sh
+++ testscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 dd if=/dev/urandom of=./file_$(date '+%Y%m%d%H%M')  bs=1M count=10
 if [ $? -eq 0 ]; then
-rsync -avz --delete -e "ssh -p 22" ./file_* devtest1@vm2-dos-linux:/home/devtest1/lesson2/
+	rsync -avz --delete -e "ssh -p 22" ./file_* devtest1@vm2-dos-linux:/home/devtest1/lesson2/
 fi
 retval=$?
 if [ $retval -eq 0 ]; then
-	find /home/devtest1/lesson2/ -name 'file_*' -mtime 0 -exec rm -f {} \;
+	rm /home/devtest1/lesson2/file_*
 fi
+ssh devtest1@vm2-dos-linux "find /home/devtest1/lesson2/ -name 'file_*' -mtime 7 -exec rm -f {} \;"
